c0184c705263a3b04033558dea77382c1249ce2d
Hannah Carnes <hecarnes@gmail.com>
2017-12-20 19:39:30 -0500

    added the ability to set image size

--- lib/iterm/imgcat.rb
+++ lib/iterm/imgcat.rb
@@ -4,10 +4,10 @@ require "base64"
 
 module Iterm
   module Imgcat
-    def get_and_print_image(url)
+    def get_and_print_image(url: nil, width: "auto", height: "auto")
       image_binary = open(url).read
       encoded_image = Base64.encode64(image_binary)
-      puts "\x1B]1337;File=inline=1:#{encoded_image}\x07"
+      puts "\x1B]1337;File=inline=1;width=#{width};height=#{height}:#{encoded_image}\x07"
     end
   end
 end
